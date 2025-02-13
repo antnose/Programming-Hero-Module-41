8945eb1a873f3edc31f6ca18e71eaa9d6e3b22c9
antnose <antnose28@gmail.com>
2025-02-13 14:05:55 +0600

    Complete 1st video of 41 module

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h3>Module Introduction ES6 Modules and data storage integration</h3>
+    </>
+  );
+}
+
+export default App;
